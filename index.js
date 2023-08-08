d382f5df1fa0286857a2f064d8df076b65d9f51a
r20222 <101579892+r20222@users.noreply.github.com>
2023-08-08 16:37:29 +0200

    fotos in rover page

--- index.js
+++ index.js
@@ -33,11 +33,51 @@ app.get('/', (request, response) => {
 // Maak een route voor de rover pagina
 app.get('/rover.ejs', (request, response) => {
   const rover = request.query.rover;
-  const url = `https://api.nasa.gov/mars-photos/api/v1/manifests/${rover}?api_key=${process.env.marsRoverKey}`;
+  const roverUrl = `https://api.nasa.gov/mars-photos/api/v1/manifests/${rover}?api_key=${process.env.marsRoverKey}`;
 
-  fetchJson(url).then((data) => {
-    response.render('rover', {data: data})
-  })
+
+  const sol = request.query.sol;
+  // const solUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=${sol}&api_key=${process.env.marsRoverKey}`;
+
+  // const solUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=${sol}&api_key=${process.env.marsRoverKey}`;
+
+
+  // Promise.all([fetchJson(roverUrl), fetchJson(solUrl)])
+  // .then(([roverData, solData]) => {
+  //   response.render('index', {roverData: roverData, solData: solData, rover: rover})
+  // }) 
+  // .catch(error => {
+  //   console.error('Fout bij het ophalen van gegevens:', error);
+  //   response.render('error', { error: error }); // Toon een foutpagina met de foutinformatie
+  // });
+
+
+  let solUrl = null;
+
+  fetchJson(roverUrl)
+    .then(roverData => {
+      const maxSol = roverData.photo_manifest.max_sol;
+
+      if (sol) {
+        solUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=${sol}&api_key=${process.env.marsRoverKey}`;
+      } else {
+        // Gebruik maxSol als standaardwaarde
+        solUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=${maxSol}&api_key=${process.env.marsRoverKey}`;
+      }
+
+      return fetchJson(solUrl)
+        .then(solData => {
+          response.render('rover', { roverData: roverData, solData: solData, rover: rover });
+        });
+    })
+    .catch(error => {
+      console.error('Fout bij het ophalen van gegevens:', error);
+      response.render('error', { error: error });
+    });
+  // fetchJson(roverUrl).then((data) => {
+  //   response.render('rover', {data: data, rover: rover})
+  //   console.log(data)
+  // })
 })
 
 // Stel het poortnummer in en start express
@@ -53,3 +93,33 @@ async function fetchJson(url) {
         .then((response) => response.json())
         .catch((error) => error)
     } 
+
+
+
+
+
+
+
+
+
+// hiermee check je het limiet
+
+    fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/curiosity?api_key=${process.env.marsRoverKey}`, {
+  method: 'GET',
+  headers: {
+    'Authorization': 'Bearer YOUR_API_KEY'
+  }
+})
+.then(response => {
+  // Controleer de X-RateLimit-Limit en X-RateLimit-Remaining headers
+  const limit = response.headers.get('X-RateLimit-Limit');
+  const remaining = response.headers.get('X-RateLimit-Remaining');
+
+  console.log('Limiet:', limit);
+  console.log('Resterend:', remaining);
+
+  return response.json(); // Verwerk de API-respons zoals nodig
+})
+.catch(error => {
+  console.error('Fout bij het ophalen van gegevens:', error);
+});
