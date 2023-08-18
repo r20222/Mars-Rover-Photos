c538cbf7c0248f1f30a92698701953ce02436ac4
r20222 <101579892+r20222@users.noreply.github.com>
2023-08-18 20:43:41 +0200

    desktop pointers

--- public/script.js
+++ public/script.js
@@ -6,7 +6,10 @@ function elementSupportsAttribute(attribute) {
 
   popover.forEach(popover => {
     if (attribute in popover) {
+      const popoverButton = document.querySelector('.mars-popover-available-button')
+
       console.log(true);
+      popoverButton.classList.add('popover-available-pointer')
     } else {
       console.log(false);
    popover.classList.add('popover-not-working')
@@ -14,3 +17,31 @@ function elementSupportsAttribute(attribute) {
   })
   
 };
+
+
+
+// openai testest
+
+// Voeg dit toe aan je bestaande JavaScript-code of een apart JavaScript-bestand
+
+// const chatForm = document.getElementById('chat-form');
+// const userInput = document.getElementById('user-input');
+// const chatOutput = document.getElementById('chat-output');
+
+// chatForm.addEventListener('submit', async (event) => {
+//   event.preventDefault();
+
+//   const userMessage = userInput.value;
+
+  // Voer hier de code uit om de chatbot-aanroep te doen en de reactie te verkrijgen
+  // Gebruik de eerder besproken API-aanroep en verwerk de chatbot-reactie
+
+  // Voeg de gebruikersinvoer en chatbot-reactie toe aan de pagina
+  // chatOutput.innerHTML += `
+  //   <div class="user-message">${userMessage}</div>
+  //   <div class="bot-message">${chatOutput}</div>
+  // `;
+
+  // Wis het invoerveld
+//   userInput.value = '';
+// });
